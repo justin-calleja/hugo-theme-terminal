8ab322e50b0e39df240ff7b4fe62e51c426c3bbe
Justin Calleja <calleja.justin@gmail.com>
2022-07-06 10:19:04 +0200

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,6 @@ module.exports = {
   // Scorll a bit past:
   // https://tailwindcss.com/docs/configuration#core-plugins
   // to find a list of available corePlugins.
-  corePlugins: ["alignSelf", "display", "height", "margin", "width"],
+  corePlugins: ["alignSelf", "display", "height", "margin", "textColor", "width"],
   important: true,
 };
